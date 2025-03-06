7fac664978dc7aa7aca1347773dba1cda2e0ccce
mtgd24 <hoangcaophong2001@gmail.com>
2025-03-06 16:54:25 +0800

    Initial Commit

---
+++ homesecurity demo 4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  homesecurity demo 4
+//
+//  Created by MTGD on 2025/3/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ homesecurity demo 4/homesecurity_demo_4App.swift
@@ -0,0 +1,17 @@
+//
+//  homesecurity_demo_4App.swift
+//  homesecurity demo 4
+//
+//  Created by MTGD on 2025/3/6.
+//
+
+import SwiftUI
+
+@main
+struct homesecurity_demo_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
